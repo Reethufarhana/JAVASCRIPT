95808a584a550f0ab2049d7f508a401d659ed2c8
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-04-24 12:56:27 +0000

    added stack and heap example

---
+++ datatype.js
@@ -0,0 +1,38 @@
+// Stack and Heap Concept
+
+// Stack which takes the copy of the first declared datatype value
+let user_name = 'Reethu';
+let  another_user_name = user_name; // here it takes the copy of the first declared value
+
+another_user_name = "Farhana";
+
+console.log(user_name); //here it wont udate any value and displays as it is declared
+
+console.log(another_user_name); //here it displays the second given value
+
+
+// Heap is a different space for storing data where JavaScript stores objects and functions. here it takes the reference of the declared datatype
+
+let userOne = {
+    name: 'reethu',
+    age: '29'
+}
+
+// const person = {...userOne}; //here it wont change the first declared values
+
+const person = userOne
+person.name = "Jaipal";
+
+console.log(userOne);
+
+const arr = [10,35,7,4,20,5];
+
+function reverse (arr){
+   let output = [];
+   for(let i = arr.length - 1; i > -1; i--){
+          output.push(arr[i])
+   }
+   return output;
+}
+
+console.log(reverse(arr));
