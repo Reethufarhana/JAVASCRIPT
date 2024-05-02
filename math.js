f4d7f34764052b0ba4424fe0bf0bd4c79747c789
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-05-02 12:45:52 +0000

    upadted changes

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
 console.log(Math); //it displays as object
 console.log(Math.abs(-4)); //output: it converts negative value to positive
-console.log(Math.round(5.2)); //output: it displays round figure number
+console.log(Math.round(5.4)); //output: it displays round figure number
 console.log(Math.floor(6.6)) //output: it displays 6 even the value after decimal is above 5
 console.log(Math.ceil(7.2));//output: it displays 8 evne the  value after decimal is below 5
