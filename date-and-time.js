4151fa87375e075e399f34df7e686bf4c5d9b310
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-05-09 10:38:04 +0000

    date file updated

--- date-and-time.js
+++ date-and-time.js
@@ -25,12 +25,17 @@ let myDate = new Date();
 // console.log(formattedDate.toLocaleString()) // displays date in mm/dd/yyyy format and time in hh:mm:ss format 
 
 // <!----------------------- Time Concept ---------------->
-let myTime = Date.now()
-console.log(myTime);  // whisch display time in milliseconds from (1st jan 1970)
+// let myTime = Date.now()
+// console.log(myTime);  // whisch display time in milliseconds from (1st jan 1970)
 
-// let formattedDate = new Date("01-14-2024"); // here month wont starts in index format 
-// console.log(formattedDate.getTime())
+// // let formattedDate = new Date("01-14-2024"); // here month wont starts in index format 
+// // console.log(formattedDate.getTime())
 
-console.log(myTime/1000); // which converts milliseconds to seconds gives in decimal point
+// console.log(myTime/1000); // which converts milliseconds to seconds gives in decimal point
 
-console.log(Math.floor(myTime/1000)); // here it removes decimal
+// console.log(Math.floor(myTime/1000)); // here it removes decimal
+
+let newDate = new Date()
+console.log(newDate.getMonth()) // here it takes the month in index form
+console.log(newDate.getMonth() + 1) // here it shows the exact month
+console.log(newDate.getDay()) // here it takes the day in index form
