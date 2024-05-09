078d992c9a7e1746fa579dc48bbf3a8eba4a2d24
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-05-09 09:19:23 +0000

    dates file added

---
+++ dates.js
@@ -0,0 +1,22 @@
+let myDate = new Date();
+// console.log(myDate); // displays date in unreadable format
+
+// console.log(myDate.toString()); // displays in readable format
+
+// console.log(myDate.toDateString()); // displays only date(day month date year)format it wont display time
+
+// console.log(myDate.toLocaleDateString()); // displays in mm/dd/yyyy format it wont display time
+
+console.log(myDate.toLocaleString()); // displays in mm/dd/yyyy format it will display time
+
+// console.log(myDate.toISOString()); // displays in yyyy-mm-dd format with time
+
+// console.log(myDate.toJSON()); // displays date in unreadable format
+
+//console.log(typeof(myDate)); // it is object
+
+let userDate = new Date(2023, 0, 14) // here it should be declare in year month and date format.. index starts from 0 so it takes jan as index 0
+console.log(userDate.toDateString());
+
+let createdDate = new Date(2023, 1, 23, 10, 15);
+console.log(createdDate.toLocaleString()); // displays date in mm/dd/yyyy format and time in hh:mm:ss format
