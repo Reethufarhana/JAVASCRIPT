7ef21a5242ebf425f2721e6e170facceac410347
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-05-31 11:57:31 +0000

    constructor-objects file uploaded

---
+++ objects-2.js
@@ -0,0 +1,9 @@
+// const obj = new Object() //  we can declare object in this way too. this is object constructor. this is singleton object.
+// console.log(obj);
+
+const obj = {}
+obj.id = '01'
+obj.name = 'reethu'
+obj.email = 'reethufarhana201@gmail.com'
+obj.employee = true
+console.log(obj);
