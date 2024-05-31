6d77358c12a57c87911488a6adcfc1e1669091f2
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-05-31 13:16:00 +0000

    constructor-objects file uploaded

--- objects-2.js
+++ objects-2.js
@@ -1,9 +1,22 @@
 // const obj = new Object() //  we can declare object in this way too. this is object constructor. this is singleton object.
 // console.log(obj);
 
-const obj = {}
-obj.id = '01'
-obj.name = 'reethu'
-obj.email = 'reethufarhana201@gmail.com'
-obj.employee = true
-console.log(obj);
+// const obj = {}
+// obj.id = '01'
+// obj.name = 'reethu'
+// obj.email = 'reethufarhana201@gmail.com'
+// obj.employee = true
+// console.log(obj);
+
+/*----------------nested object----------------*/
+
+const anotherObj = {
+    email: 'obj@gmail.com',
+    name: {
+        userName: {
+            firstName: 'reethu',
+            lastName: 'farhana'
+        }
+    }
+}
+console.log(anotherObj.name); // 
