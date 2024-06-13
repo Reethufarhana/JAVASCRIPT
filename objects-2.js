354347f5e6dfb782a1996ac3c584c0f75162af26
Reethufarhana <30995411+Reethufarhana@users.noreply.github.com>
2024-06-13 10:11:24 +0000

    Objects-2 updated

--- objects-2.js
+++ objects-2.js
@@ -1,22 +1,53 @@
-// const obj = new Object() //  we can declare object in this way too. this is object constructor. this is singleton object.
-// console.log(obj);
+// const obj = new Object()
+// console.log(obj)
 
 // const obj = {}
-// obj.id = '01'
 // obj.name = 'reethu'
 // obj.email = 'reethufarhana201@gmail.com'
-// obj.employee = true
 // console.log(obj);
 
-/*----------------nested object----------------*/
-
 const anotherObj = {
-    email: 'obj@gmail.com',
+    id: '01',
     name: {
-        userName: {
+        fullName: {
             firstName: 'reethu',
-            lastName: 'farhana'
+            lastName: 'farhana',
         }
     }
 }
-console.log(anotherObj.name); // 
+console.log(anotherObj['name']); 
+//(or)
+console.log(anotherObj.name);
+console.log(anotherObj.name.fullName);
+
+// merging the objects by using 'assign'
+
+const obj1 = {1: 'a', 2:'b'}
+const obj2 = {3: 'c', 4:'d'}
+console.log(Object.assign({}, obj1, obj2)) // here {} as target object and remaing are source object. this source objects will stored in one target object and displays as a new object. here assign is used to merge two or more objetcs.
+
+//(or) we can merge two or more objects by using spread operator
+
+console.log({...obj1, ...obj2});
+
+// here we can display only keys or only values
+
+console.log(Object.keys(obj1)); //here it displays keys of the obj1 object and the datatype is array
+console.log(Object.values(obj1)); //here it displays values of the obj1 object and the datatype is array
+console.log(Object.entries(obj1)); // here it displays each key and value into an array "like[[],[]]"
+console.log(obj1.hasOwnProperty('1')); //here it checks weather given property is there or not in object obj1 and returns true or false.
+// ------------------------------------------------------------------------------------------------//
+
+// we can create objects in array
+
+const arrObj = [
+    {
+        id: '01',
+        name: 'abc'
+    },
+    {
+        id: '02',
+        name: 'xyz'
+    }
+]
+console.log(arrObj[0].id); //array starts with index 0. so it takes 0th index object values
